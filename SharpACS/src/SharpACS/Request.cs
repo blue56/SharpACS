867991029a1fac31abb2a5dc0c4333502c159487
Jacob Kofod <jacob_kofod@yahoo.com>
2024-12-22 07:09:09 +0100

    Removed old request type

--- SharpACS/src/SharpACS/Request.cs
+++ SharpACS/src/SharpACS/Request.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 using SharpACS;
 
 [JsonDerivedType(typeof(SummaryRequest), typeDiscriminator: "Summary")]
-[JsonDerivedType(typeof(AWSCostExplorerRequest), typeDiscriminator: "Explorer")]
 public abstract class Request
 {
 
